e872cad7717b12885d6673d17dfe58c84e50f235
saymahtab <reachmahtabsce@gmail.com>
2024-08-04 10:41:45 +0530

    added animation

--- script.js
+++ script.js
@@ -1,101 +1,118 @@
 const board = document.querySelector('.game-board');
 const msgBox = document.querySelector('.message');
-const msgBoxCont = document.querySelector('.msg-container')
+const msgBoxCont = document.querySelector('.msg-container');
 const newButton = document.querySelector('.new-button');
 const resetButton = document.querySelector('.reset-button');
 
-const arr = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
-]
+const clickSound = document.getElementById('click-sound');
+const winSound = document.getElementById('win-sound');
+const loseSound = document.getElementById('lose-sound');
+const tieSound = document.getElementById('tie-sound');
 
-for(let i=0; i<9; i++) {
+const arr = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+for (let i = 0; i < 9; i++) {
     const box = document.createElement('button');
     box.setAttribute('class', 'box');
-    board.appendChild(box)
+    board.appendChild(box);
 }
+
 const boxArray = document.querySelectorAll('.box');
 let flag = true;
 
 boxArray.forEach(box => {
-    box.addEventListener(('click'), () => {
-        if(flag) {
-            box.innerText = 'X';
-        }
-        else {
-            box.innerText = 'O'
-        }
-        flag = !flag
-        box.disabled = 'true'
-        checkWin(); 
-        const a = checkTie();
-        if(a){
-            msgBox.innerText = `No Winner. Play Again`;
-            msgBoxCont.classList.remove('hide')
-        }
-    })
-})
-
-const checkTie = () => {
-    let tie = true;
-    for(let box of boxArray) {
-        if(box.innerText == ''){
-            tie=false;
-        }
-    }
-    return tie;
-}
-
-const disableBox = () => {
-    for(let box of boxArray) {
-        box.disabled = true;
-    }
-}
-const enableBox = () => {
-    for(let box of boxArray) {
-        box.disabled = false;
-    }
-}
-newButton.addEventListener(('click'), () => {
-    msgBoxCont.classList.add('hide');
-    flag = true;
-    boxArray.forEach((box) => {
-        box.innerText = '';
-    })
-    enableBox();
-})
-
-resetButton.addEventListener(('click'), () => {
-    msgBoxCont.classList.add('hide');
-    flag = true;
-    boxArray.forEach((box) => {
-        box.innerText = '';
-    })
-    enableBox();
-})
-
-const checkWin = () => {
-    arr.forEach(arr => {
-        let pos1 = boxArray[arr[0]].innerText;
-        let pos2 = boxArray[arr[1]].innerText;
-        let pos3 = boxArray[arr[2]].innerText;
-
-        if(pos1 != '' && pos2 != '' && pos3 != '') {
-            if(pos1 == pos2 && pos2 == pos3) {
-                showWinner(pos1);
+    box.addEventListener('click', () => {
+        if (box.innerText === '') {
+            clickSound.play();
+            box.innerText = flag ? 'X' : 'O';
+            flag = !flag;
+            box.disabled = true;
+            checkWin();
+            const isTie = checkTie();
+            if (isTie) {
+                tieSound.play();
+                msgBox.innerText = `No Winner. Play Again`;
+                msgBoxCont.classList.remove('hide');
             }
         }
     });
-}
+});
 
-const showWinner = (winner) => {
-    msgBox.innerText = `Congratulations: ${winner} is the Winner`;
-    msgBoxCont.classList.remove('hide')
+const checkTie = () => {
+    return [...boxArray].every(box => box.innerText !== '');
+};
+
+const disableBox = () => {
+    boxArray.forEach(box => (box.disabled = true));
+};
+
+const enableBox = () => {
+    boxArray.forEach(box => (box.disabled = false));
+};
+
+newButton.addEventListener('click', () => {
+    msgBoxCont.classList.add('hide');
+    flag = true;
+    boxArray.forEach(box => {
+        box.innerText = '';
+        box.classList.remove('winner');
+    });
+    enableBox();
+});
+
+resetButton.addEventListener('click', () => {
+    msgBoxCont.classList.add('hide');
+    flag = true;
+    boxArray.forEach(box => {
+        box.innerText = '';
+        box.classList.remove('winner');
+    });
+    enableBox();
+});
+
+const checkWin = () => {
+    arr.forEach(combo => {
+        let [a, b, c] = combo;
+        if (
+            boxArray[a].innerText &&
+            boxArray[a].innerText === boxArray[b].innerText &&
+            boxArray[a].innerText === boxArray[c].innerText
+        ) {
+            showWinner(boxArray[a].innerText);
+            boxArray[a].classList.add('winner');
+            boxArray[b].classList.add('winner');
+            boxArray[c].classList.add('winner');
+        }
+    });
+};
+
+const showWinner = winner => {
+    msgBox.innerText = `Congratulations: ${winner} is the Winner!`;
+    msgBoxCont.classList.remove('hide');
     disableBox();
-}
+    winSound.play();
+};
+
+// Winner animation
+const winnerAnimation = document.createElement('style');
+winnerAnimation.innerHTML = `
+  @keyframes winner {
+    0% { transform: scale(1); }
+    50% { transform: scale(1.2); }
+    100% { transform: scale(1); }
+  }
+
+  .winner {
+    animation: winner 0.5s ease-in-out infinite;
+  }
+`;
+document.head.appendChild(winnerAnimation);
